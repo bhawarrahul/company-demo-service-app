bd5573a794ad0ebad6fc7bc0c7ee412f030e3ad1
rahul.bhawar <rahul.bhawar@krawlernetworks.com>
2020-01-08 18:31:47 +0530

    Not null changes

--- src/main/java/com/product/entity/ProductCategory.java
+++ src/main/java/com/product/entity/ProductCategory.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.NotNull;
 public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
     private Long id;
     @NotNull
     private String name;
